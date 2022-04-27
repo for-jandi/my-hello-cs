74f816006cf95f536918e851a7b262405a90a4cb
m04j00 <m04j00@gmail.com>
2022-04-27 10:00:17 +0900

    #2 환영 인사 버튼 추가

--- MyHelloCs/Form1.Designer.cs
+++ MyHelloCs/Form1.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnGreeting = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // BtnGreeting
+            // 
+            this.BtnGreeting.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnGreeting.Font = new System.Drawing.Font("한컴산뜻돋움", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGreeting.Location = new System.Drawing.Point(222, 92);
+            this.BtnGreeting.Name = "BtnGreeting";
+            this.BtnGreeting.Size = new System.Drawing.Size(310, 132);
+            this.BtnGreeting.TabIndex = 0;
+            this.BtnGreeting.Text = "환영 인사 버튼";
+            this.BtnGreeting.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(797, 768);
+            this.Controls.Add(this.BtnGreeting);
             this.Name = "FormMain";
             this.Text = "Joohoney";
             this.ResumeLayout(false);
@@ -42,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnGreeting;
     }
 }
 
